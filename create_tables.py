b4dab12ab9fbc925f4bf1fda982951d72d154e28
Daniel Fields <dfieldstherunner@gmail.com>
2021-12-14 14:33:25 -0800

    Add docstrings

--- create_tables.py
+++ create_tables.py
@@ -3,7 +3,19 @@ import psycopg2
 from sql_queries import create_table_queries, drop_table_queries, create_schema_queries
 
 def create_schemas(cur,conn):
-    """This will create the staging and star schemas in the redshift cluster if the schemas do not already exist."""
+    """
+        Function Purpose: 
+            This function executes the CREATE SCHEMA statements imported from the sql_queries.py script. 
+            This function will create a staging schema (DWH.STAGING) and star schema (DWH.STAR)
+            in the DWH Redshift database.
+            
+        Args:
+            cur (psycopg2.connection): A psycopg2 PostgreSQL connection cursor.
+            conn (psycopg2.connection.cursor): A psycopg2 PostgreSQL connection cursor.
+
+        Returns:
+            None
+    """
     for query in create_schema_queries:
         print(query)
         cur.execute(query)
@@ -11,6 +23,20 @@ def create_schemas(cur,conn):
         print('Success.')
 
 def drop_tables(cur, conn):
+    """
+        Function Purpose: 
+            This function executes DROP TABLE statements imported from the sql_queries.py script. 
+            This is done in order to ensure that the ETL proccess will insert data into empty tables
+            so we first drop all the tables in the database before creating them again.
+            
+        Args:
+            cur (psycopg2.connection): A psycopg2 PostgreSQL connection cursor.
+            conn (psycopg2.connection.cursor): A psycopg2 PostgreSQL connection cursor.
+
+        Returns:
+            None
+    """
+    
     for query in drop_table_queries:
         print(query)
         cur.execute(query)
@@ -18,6 +44,20 @@ def drop_tables(cur, conn):
         print('Success.')
 
 def create_tables(cur, conn):
+    """
+        Function Purpose: 
+            This function executes DROP TABLE statements imported from the sql_queries.py script. 
+            This is done in order to ensure that the ETL proccess will insert data into empty tables
+            so we first drop all the tables in the database before creating them again.
+            
+        Args:
+            cur (psycopg2.connection): A psycopg2 PostgreSQL connection cursor.
+            conn (psycopg2.connection.cursor): A psycopg2 PostgreSQL connection cursor.
+
+        Returns:
+            None
+    """
+    
     for query in create_table_queries:
         print(query)
         cur.execute(query)
@@ -26,6 +66,18 @@ def create_tables(cur, conn):
 
 
 def main():
+    """
+        Function Purpose: 
+            The main function in this script will connect to the Redshift PostgreSQL data warehouse 
+            and then call the drop_tables and create_tables to set up the database for ETL.
+            Then this function will close the connection to the database.
+        Args:
+            None
+
+        Returns:
+            None
+    """
+    
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
